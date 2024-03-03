205537e4b7cfc2226b8233bcf3356dbf2b62e1b7
developer.rahmani <developer.rahmani@gmail.com>
2024-03-03 13:18:06 +0330

    update tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   const [isShow, setIsShow] = useState(false);
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div>
       <button
-        className="bg-gray-900 py-2 px-4 rounded-[8px]"
+        className="bg-gray-900 py-2 px-4 rounded-[8px] text-gray-200 mt-4"
         onClick={() => setIsShow((prevState) => !prevState)}
       >
         toggle
